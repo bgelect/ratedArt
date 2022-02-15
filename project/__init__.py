1ada06309c03c2d6c10a5cbe0dade03ec81f0d62
bgelect <bgelect@hotmail.com>
2022-02-15 17:19:18 -0600

    other changes

--- project/__init__.py
+++ project/__init__.py
@@ -35,5 +35,13 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp) 
 
+    def create_app():
+        app = ...
+    # existing code omitted
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
 
     return app
